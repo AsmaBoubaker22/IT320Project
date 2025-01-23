48c0929d18a43bfca280aefb20a19127008a885b
Asma Boubaker <154929672+AsmaBoubaker22@users.noreply.github.com>
2025-01-23 22:58:37 +0000

    Add files via upload

---
+++ businessLayerClasses/DevelopmentSite.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.it320projet;
+
+/**
+ *
+ * @author Asma
+ */
+public class DevelopmentSite extends NonResidentialProperty { 
+    private boolean underConstruction;
+
+    // Constructor
+    public DevelopmentSite(int propertyId, int landsize, Location location, boolean isLandCleared, boolean underConstruction) {
+        super(propertyId, landsize, location, isLandCleared);
+        this.underConstruction = underConstruction;
+    }
+
+    @Override
+    public String getPropertyType() {
+        return "DevelopmentSite";
+    }
+    
+    public void displayFeatures() {
+        System.out.println("Development Site Features:");
+        System.out.println("Under Construction: " + (underConstruction ? "Yes" : "No"));
+        super.displayDetails();  
+    }
+
+    public boolean isUnderConstruction() { 
+        return underConstruction; 
+    }
+
+    public void setUnderConstruction(boolean underConstruction) { 
+        this.underConstruction = underConstruction; 
+    }
+}
